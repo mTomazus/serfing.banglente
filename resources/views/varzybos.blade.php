9c45c8ec778f8b0ac1f544f27fbe28196f93df2b
Tomas Ūksas <admin@Tomass-MacBook-Pro.local>
2021-02-23 20:18:04 +0000

    page varzybos styled

--- resources/views/varzybos.blade.php
+++ resources/views/varzybos.blade.php
@@ -2,14 +2,25 @@
 
 @section('content')
 
-<div class="content varzybos">
-    <h1>VARŽYBOS</h1>
-    <p> Molas Surf 2021</p>
-    <p>Longboard Classic</p>
-    <div id="container">
-        <button class="cta-btn learn-more">Info</button>
+<div class="content varzybos bg-gradient"">
+    <div>
+        <h1>VARŽYBOS</h1>
+        <h4>Liepos 1 - Rugpjūčio 31</h4>
+        <h2>Molas Surf</h2>
+        <h2>Longboard Classic</h2>
+        <h2>2021</h2>
     </div>
+    <div>
+        <h3>Spotas "Molas"</h3>
+        <h3>Klaipėda - Melnragė</h3>
+        <h3>55°43'50.1"N 21°04'59.8"E</h3>
+        <h3>Dvi grupės</h3>
+        <h3>Max 12 dalyvių</h3>
+        <h3>Vyrai ir moterys</h3>
+        <h3>Min 9' lentos</h3>
+      </div>
     <div class="remejai">
+        <h2>REMĖJAI</h2>
         <section>
             <img src="./img/ateik.png" alt="">
             <img src="./img/oneill.png" alt="">
