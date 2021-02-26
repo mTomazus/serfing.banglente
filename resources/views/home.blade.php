9c161695ac972b31396ed14956e62459134c6280
Tomas Ūksas <admin@Tomass-MacBook-Pro.local>
2021-02-26 16:38:26 +0000

    more small for 1200 css fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,9 +11,7 @@
         <div class="s1 section"> 
             <div class="content landing">
                 <div class="heading">
-                    <h1>LAIKAS</h1>
-                    <h1>IŠMOKTI</h1>
-                    <h1>SERFINTI</h1>
+                    <h1>LAIKAS IŠMOKTI SERFINTI</h1>
                 </div>
                 <button>REGISTUOKIS</button>
                 <div class="stars">
@@ -23,16 +21,6 @@
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 6.76l1.379 4.246h4.465l-3.612 2.625 1.379 4.246-3.611-2.625-3.612 2.625 1.379-4.246-3.612-2.625h4.465l1.38-4.246zm0-6.472l-2.833 8.718h-9.167l7.416 5.389-2.833 8.718 7.417-5.388 7.416 5.388-2.833-8.718 7.417-5.389h-9.167l-2.833-8.718z"/></svg>
-                </div>
-                <div>
-                    <ul class="listas">
-                        <li>
-                            <a aria-current="page" href="#">Pamokos - Įranga</a>
-                        </li>
-                        <li>
-                            <a aria-current="page" href="#">Stovykla - Burelis - Kelionės</a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>   <!--- end SECTION 1  --->
@@ -68,10 +56,8 @@
         <div class="s3 section">
             <div class="landing__third">
                 <div class="content">
-                    <div>
-                        <h1>VASAROS</h1>
-                        <h1>DIENOS</h1>
-                        <h1>STOVYKLA</h1>
+                    <div class="heading">
+                        <h1>VASAROS DIENOS STOVYKLA</h1>
                         <h2>PENKIOS DIENOS TRENIRUOČIŲ</h2>
                         <h2>GRYNAS ORAS IR JŪRA</h2>
                     </div>                        
