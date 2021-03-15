cbc75a3c52b6540b1db7191721a168de0f8e4726
Tomas Asus Linux <tomas@banglente.com>
2021-03-15 20:52:48 +0200

    stovykla work v2

--- resources/views/stovykla.blade.php
+++ resources/views/stovykla.blade.php
@@ -15,19 +15,25 @@
             <div class="lg">
                 <div class="lg__info">
                     <div class="content-box">
-                        <p>Stovykla organizuojama nuo Birželio vidurio iki Rugpjūčio pabaigos, grupės yra riboto dydžio, todėl skambinkite arba registruokites dabar, kad gautumėte vietą norimoje pamainoje. Grupės sudaromos iš 6-8 mokančių plaukti vaikų nuo 10 iki 18 metų.</p>
+                        <p>Mes jau penktus metus veikianti banglenčių mokykla Melnragėje.</p>
+                        <p>Penktus metus organizuojame stovyklas vaikams.</p>
+                        <p>Stovykla organizuojama nuo Birželio vidurio iki Rugpjūčio pabaigos, grupės yra riboto dydžio, todėl skambinkite arba registruokites dabar, kad gautumėte vietą norimoje pamainoje. Grupės sudaromos iš 8-10 mokančių plaukti vaikų nuo 10 iki 18 metų.</p>
                         <p>Pamaina trunka 5 dienas nuo pirmadienio iki penktadienio (nuo 9:00 iki 17:30), bet galimi ir vienos ar keletos dienų variantai.</p>
                         <section>
                             <p>Pamainos kaina 170€.</p>
                             <p>Vienos dienos kaina 45€.</p>
                             <p>Maitinimas įskaičiuotas</p>
                         </section>
-                        <section>
-                            <p>Pamainos kaina 170€.</p>
-                            <p>Vienos dienos kaina 45€.</p>
-                            <p>Maitinimas įskaičiuotas</p>
-                        </section>
-                        <p>Mes jau penktus metus veikianti banglenčių mokykla Melnragėje. Ši savaitinė programa apima tiek teorines, tiek praktines žinias apie jūrą, bangas ir banglentes. Visi instruktoriai patyrę Klaipėdos gelbėtojai ir ilgametę patirtį turintys banglenčių instruktoriai, bet, svarbiausia, jie žino, kaip padaryti stovyklą nepamirštamu vasaros nuotykiu kiekvienam stovyklos dalyviui.</p>        
+                        <table>
+                            <th>Stovyklautojam suteikiame visas reikalingas sporto priemones:</th>
+                            <td>įvairaus dydžio minkštas banglentes;</td>
+                            <td>šiltus hidrokostiumus ir esant reikalui batus;</td>
+                            <td>"carver", "smoothstar" ir "classic" tipo riedlentes;</td>
+                            <td>taip pat išbandysime ir irklentes.</td>
+                        </table>
+                        <p>Ši savaitinė programa apima tiek teorines, tiek praktines žinias apie jūrą, bangas ir banglentes. Stovykla tinka tiek pirmą kartą atvykstančiam į stovyklą, tiek ir trečią ar ketvirtą. Visi instruktoriai patyrę Klaipėdos gelbėtojai ir ilgametę patirtį turintys banglenčių instruktoriai, bet, svarbiausia, jie žino, kaip padaryti stovyklą nepamirštamu vasaros nuotykiu kiekvienam stovyklos dalyviui.</p>        
+                        <p>Norint užsiregistruoti, reikia užpildyti registracijos formą ir atlikti avansini 30 Eur pavedimą į<br>"VšĮ Banglentė" sąskaitą - LT22 7300 0101 4459 4068 (swed).</p>
+                        <p>Visada mielai atsakysime į visus jums kylančius klausimus telefonu. </p>
                     </div>
                     <div class="content-side">
                         <h2>GALERIJA</h2>
@@ -102,7 +108,8 @@
                             </form>
                         </div>
                         <h2>PARSISIŲSTI</h2>
-                        <h3><a class="link" href="http://www.banglente.com/incoming/programa.pdf" target="_blank" style="display: initial">Stovyklos preliminari programa</a></h3>
+                        <h3><a class="link" href="http://www.banglente.com/incoming/programa.pdf" target="_blank" style="display: initial">Preliminari programa</a></h3>
+                        <h3><a class="link" href="http://www.banglente.com/incoming/programa.pdf" target="_blank" style="display: initial">Sutartis</a></h3>
                     </div>
                 </div>
             </div>
