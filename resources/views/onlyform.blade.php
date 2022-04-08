1d9b6c620b92b3ec95c387173b6bc424d3196c08
Tomas Asus Linux <tomas@banglente.com>
2022-04-08 22:23:39 +0300

    olyform update

--- resources/views/onlyform.blade.php
+++ resources/views/onlyform.blade.php
@@ -1,10 +1,20 @@
-@extends('layouts.form')
+@extends('layouts.app')
 
 @section('content')
+
+    <div class="my-auto d-block py-3">
+        <img class="mx-auto d-block" style="width:200px" src="/img/naujas-title.png" alt="banglente logo">
+        <h2 class="text-dark mt-2">2022 Vasaros stovyklos registracija</h2>
+    </div>
 
     <div class="col-12 mx-auto">
         @livewire('stovykla-form')
     </div>
+
+    <h2 class="mt-4 text-dark">VšĮ Banglentė</h2>
+    <h2 class="text-dark">Vėtros g. 8, Klaipėda</h2>
+    <h2 class="text-dark">Įmonės kodas - 304105037</h2>
+    <h2 class="text-dark">Bankas - LT227300010144594068</h2>
            
 @endsection
 
